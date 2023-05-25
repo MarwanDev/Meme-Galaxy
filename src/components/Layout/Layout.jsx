4a8d72e179c9ffe01a284e343a8130a5c50df241
MarwanDev <marwaneg@aucegypt.edu>
2023-05-25 23:50:34 +0300

    Fix errors

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,3 +1,6 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { Route, Routes } from 'react-router-dom';
 import { Home, Details } from '../../pages';
 
